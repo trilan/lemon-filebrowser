a69144cc10eefe9bc0cd0b739621300af22d25ca
Mike Yumatov <mike@yumatov.org>
2012-08-21 09:13:55 +0400

    Bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(filename):
 
 setup(
     name='lemon-filebrowser',
-    version='0.1.dev',
+    version='0.1',
     license='ISC',
     description="Fork of Patrick Kranzlmueller's django-filebrowser app.",
     url='https://github.com/trilan/lemon-filebrowser',
